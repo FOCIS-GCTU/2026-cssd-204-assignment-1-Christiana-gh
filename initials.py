f34edd0744771267efb44efd83ab9d04434e8043
unknown <christinamartey587@gmail.com>
2026-05-30 16:37:46 +0000

    Complete assignment with required automated test scripts

---
+++ initials.py
@@ -0,0 +1,34 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Ms Christiana Martey
+# STUDENT ID: 2425400931
+# Email: 2425400931@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Christiana Martey, this programming assignment is my own
+# work and I have not provided this code to any other student.
+
+
+def main():
+    # Print stylized initials
+
+    print()
+    print("...MCM")
+    print()
+
+    print("...MMM......MMM........CCCCCCCCCCCC........MMM......MMM.....")
+    print("...MMM......MMM........CCC.................MMMM....MMMM.....")
+    print("...MMM.MMMM.MMM........CCC.................MMM.MMMM.MMM.....")
+    print("...MMM..MM..MMM........CCC.................MMM..MM..MMM.....")
+    print("...MMM......MMM........CCC.................MMM......MMM.....")
+    print("...MMM......MMM........CCC.................MMM......MMM.....")
+    print("...MMM......MMM........CCC.................MMM......MMM.....")
+    print("...MMM......MMM........CCC.................MMM......MMM.....")
+    print("...MMM......MMM...**...CCC............**...MMM......MMM...**")
+    print("...MMM......MMM...**...CCCCCCCCCCCC...**...MMM......MMM...**")
+    print()
+
+
+main()
